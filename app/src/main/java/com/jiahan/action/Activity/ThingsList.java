55abf76e3c8eea67eb6bfbed7ed2f0e59d6fce57
gitHubJiahan <work_github@outlook.com>
2015-10-27 14:29:18 +0800

    27/10

--- app/src/main/java/com/jiahan/action/Activity/ThingsList.java
+++ app/src/main/java/com/jiahan/action/Activity/ThingsList.java
@@ -1,6 +1,5 @@
 package com.jiahan.action.Activity;
 
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,28 +7,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-import com.jiahan.action.Dao.Book;
-
-import com.jiahan.action.Dao.BookDao;
-import com.jiahan.action.Dao.DaoMaster;
-import com.jiahan.action.Dao.DaoSession;
 import com.jiahan.action.R;
 
-public class AddBookActivity extends AppCompatActivity {
-    private SQLiteDatabase db;
-    private DaoMaster daoMaster;
-    private DaoSession daoSession;
-    private BookDao bookDao;
+public class ThingsList extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        DaoMaster.DevOpenHelper helper = new DaoMaster.DevOpenHelper(this, "life-db", null);
-        db = helper.getWritableDatabase();
-        daoMaster = new DaoMaster(db);
-        daoSession = daoMaster.newSession();
-        bookDao = daoSession.getBookDao();
-        setContentView(R.layout.activity_addbook);
+        setContentView(R.layout.activity_things_list);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -37,10 +22,6 @@ public class AddBookActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                Book book = new Book();
-
-                bookDao.insert(book);
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
---
+++ app/src/main/java/com/jiahan/action/Adapter/BookAdapter.java
@@ -0,0 +1,79 @@
+package com.jiahan.action.Adapter;
+
+import android.content.Context;
+import android.database.DatabaseUtils;
+import android.databinding.DataBindingUtil;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.jiahan.action.Dao.Book;
+import com.jiahan.action.R;
+import com.jiahan.action.databinding.BooksItemBinding;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Administrator on 2015/10/22 0022.
+ */
+public class BookAdapter extends RecyclerView.Adapter<BookAdapter.BookViewHolder> {
+
+    public static final int LAST_POSTION = -1 ;
+
+    private final List<Book> mData ;
+
+    public static BookAdapter newInstance()
+    {
+          List<Book> books = new ArrayList<Book>();
+          return new BookAdapter(books);
+    }
+
+    public BookAdapter(List<Book> mData) {
+        this.mData = mData;
+    }
+
+    @Override
+    public BookViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater  inflater = LayoutInflater.from(context);
+        View booksContainer = inflater.inflate(R.layout.books_item,parent,false);
+        return new BookViewHolder(booksContainer);
+    }
+
+    @Override
+    public void onBindViewHolder(BookViewHolder holder, int position) {
+        Book book= mData.get(position);
+        holder.bind(book);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mData.size();
+    }
+
+    public void setBooks(List<Book> books)
+    {
+       this.mData.clear();
+        this.mData.addAll(books);
+        notifyDataSetChanged();
+
+    }
+
+    public static class BookViewHolder extends RecyclerView.ViewHolder
+    {
+
+      private BooksItemBinding binding;
+        public BookViewHolder(View itemView) {
+            super(itemView);
+            binding = DataBindingUtil.bind(itemView);
+        }
+        public void bind(Book book)
+        {
+            binding.setBook(book);
+        }
+
+    }
+}
